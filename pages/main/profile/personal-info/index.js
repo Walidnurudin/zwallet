1db05e4588c117014b0b282c4d5248546ecdbd54
Walidnurudin <walidnurudin47@gmail.com>
2021-11-19 18:07:37 +0700

    Integrasi: Update noTelp user. Slicing: personal-info page

--- pages/main/profile/personal-info/index.js
+++ pages/main/profile/personal-info/index.js
@@ -1,8 +1,98 @@
+import React, { useState } from "react";
 import MainLayout from "components/layouts/MainLayout";
+import { getDataCookie } from "middleware/authorizationPage";
+import axios from "utils/axios";
+import { Modal } from "react-bootstrap";
+import { Button } from "components/module";
 
-export default function PersonalInfo() {
+export async function getServerSideProps(context) {
+  const dataCookie = await getDataCookie(context);
+  if (!dataCookie.isLogin) {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
+
+  // GET USER
+  const user = await axios
+    .get(`/user/profile/${dataCookie.id}`, {
+      headers: {
+        Authorization: `Bearer ${dataCookie.token}`,
+      },
+    })
+    .then((res) => {
+      return res.data.data;
+    })
+    .catch((err) => {
+      console.log(err.response);
+      return [];
+    });
+
+  return {
+    props: {
+      data: { dataCookie, user },
+    },
+  };
+}
+
+export default function PersonalInfo(props) {
+  const [data, setData] = useState(props.data.user);
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [noTelp, setNoTelp] = useState("");
+
+  const handleText = (e) => {
+    setNoTelp(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    axios
+      .patch(`/user/profile/${props.data.dataCookie.id}`, { noTelp: noTelp })
+      .then((res) => {
+        console.log(res);
+        notifSuccess(res.data.msg);
+        axios
+          .get(`/user/profile/${props.data.dataCookie.id}`)
+          .then((res) => {
+            console.log(res.data.data);
+            setData(res.data.data);
+          })
+          .catch((err) => {
+            console.log(err.response);
+          });
+        handleClose();
+      })
+      .catch((err) => {
+        console.log(err.response);
+      });
+  };
+
+  // // GET USER
+  // const getUser = () => {
+  //   axios
+  //     .get(`/user/profile/${props.dataCookie.id}`)
+  //     .then((res) => {
+  //       console.log(res.data.data)
+  //     })
+  //     .catch((err) => {
+  //       console.log(err.response);
+  //     });
+  // };
+
   return (
-    <MainLayout title="Personal Info" firstName="walid" lastName="Nurudin">
+    <MainLayout
+      title="Personal Info"
+      firstName={data.firstName}
+      lastName={data.lastName}
+      noTelp={data.noTelp}
+      image={data.image}
+    >
       <div
         style={{
           padding: "30px",
@@ -13,8 +103,132 @@ export default function PersonalInfo() {
           background: "#ffffff",
         }}
       >
-        Personal Info
+        <h5 className="nunito-700">Personal Info</h5>
+        <p
+          className="nunito-400 font-secondary"
+          style={{
+            width: "40%",
+            marginTop: "25px",
+            marginBottom: "40px",
+            lineHeight: "28px",
+          }}
+        >
+          We got your personal information from the sign up proccess. If you
+          want to make changes on your information, contact our support.
+        </p>
+
+        <div
+          className="d-flex"
+          style={{
+            boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.05)",
+            borderRadius: "10px",
+            padding: "20px",
+            marginTop: "25px",
+            marginBottom: "20px",
+          }}
+        >
+          <div>
+            <div>
+              <span className="nunito-400 font-thrid">First Name</span>
+              <h5 className="nunito-600 mt-2">{data.firstName}</h5>
+            </div>
+          </div>
+        </div>
+
+        <div
+          className="d-flex"
+          style={{
+            boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.05)",
+            borderRadius: "10px",
+            padding: "20px",
+            marginTop: "25px",
+            marginBottom: "20px",
+          }}
+        >
+          <div>
+            <div>
+              <span className="nunito-400 font-thrid">Last Name</span>
+              <h5 className="nunito-600 mt-2">{data.lastName}</h5>
+            </div>
+          </div>
+        </div>
+
+        <div
+          className="d-flex"
+          style={{
+            boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.05)",
+            borderRadius: "10px",
+            padding: "20px",
+            marginTop: "25px",
+            marginBottom: "20px",
+          }}
+        >
+          <div>
+            <div>
+              <span className="nunito-400 font-thrid">Verified E-mail</span>
+              <h5 className="nunito-600 mt-2">{data.email}</h5>
+            </div>
+          </div>
+        </div>
+
+        <div
+          className="d-flex justify-content-between"
+          style={{
+            boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.05)",
+            borderRadius: "10px",
+            padding: "20px",
+            marginTop: "25px",
+            marginBottom: "20px",
+          }}
+        >
+          <div>
+            <div>
+              <span className="nunito-400 font-thrid">Phone Number</span>
+              <h5 className="nunito-600 mt-2">{data.noTelp || "-"}</h5>
+            </div>
+          </div>
+          <div
+            className="align-self-center nunito-400 font-primary"
+            onClick={handleShow}
+            style={{ cursor: "pointer" }}
+          >
+            Manage
+          </div>
+        </div>
       </div>
+
+      {/* UPDATE HO TELP */}
+      <Modal show={show} onHide={handleClose} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Phone Number</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p className="nunito-400 font-secondary">
+            Enter phone number to update your profile
+          </p>
+          <input
+            type="number"
+            name="noTelp"
+            placeholder="Input Phone Number ..."
+            onChange={handleText}
+            className="nunito-700 font-primary"
+            style={{
+              height: "70px",
+              width: "100%",
+              textAlign: "center",
+              fontSize: "20px",
+              marginTop: "40px",
+              marginBottom: "50px",
+              border: "1px solid rgba(169, 169, 169, 0.6)",
+              borderRadius: "10px",
+              outline: "none",
+            }}
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button name="Submit" width="170px" handleClick={handleSubmit} />
+        </Modal.Footer>
+      </Modal>
     </MainLayout>
   );
 }
