4e5a20904e59132d943b50d788913c0deccbca5b
Walidnurudin <walidnurudin47@gmail.com>
2021-11-22 02:50:10 +0700

    conditional rendering landing-page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 /* eslint-disable @next/next/no-img-element */
+import { useRouter } from "next/router";
+import Cookie from "js-cookie";
+
 export default function Home() {
+  const router = useRouter();
+
   return (
     <div style={{ backgroundColor: "#E5E5E5" }}>
       {/* HEADER */}
@@ -7,8 +12,31 @@ export default function Home() {
         <img src="../assets/images/logo/zwallet.png" alt="logo" />
 
         <div>
-          <button className="btn btn-outline-primary">Login</button>
-          <button className="btn btn-primary">Sign Up</button>
+          {Cookie.get("token") ? (
+            <>
+              <button
+                className="btn btn-primary"
+                onClick={() => router.push("/home")}
+              >
+                Dashboard
+              </button>
+            </>
+          ) : (
+            <>
+              <button
+                className="btn btn-outline-primary"
+                onClick={() => router.push("/login")}
+              >
+                Login
+              </button>
+              <button
+                className="btn btn-primary"
+                onClick={() => router.push("/register")}
+              >
+                Sign Up
+              </button>
+            </>
+          )}
         </div>
       </section>
 
@@ -51,7 +79,9 @@ export default function Home() {
       </section>
 
       {/* STARTUP */}
-      <section style={{ background: "rgba(71, 58, 209, 0.06)" }}>
+      <section
+        style={{ background: "rgba(71, 58, 209, 0.06)", padding: "90px 0px" }}
+      >
         <div className="container d-flex justify-content-between">
           <img
             src="../assets/images/landing-page/microsoft.png"
@@ -87,13 +117,131 @@ export default function Home() {
       </section>
 
       {/* ABOUT */}
-      <section>About</section>
+      <section className="container text-center">
+        <h1 className="nunito-700">
+          <span className="font-primary">About</span> the Application.
+        </h1>
+
+        <p>
+          We have some great features from the application and it’s totally free
+          to use by all users around the world.
+        </p>
+
+        <div className="d-flex">
+          <div>
+            <img
+              src="../assets/images/landing-page/phone.png"
+              alt="phone"
+              width="60px"
+            />
+            <h5>24/7 Support</h5>
+            <p>
+              We have 24/7 contact support so you can contact us whenever you
+              want and we will respond it.
+            </p>
+          </div>
+
+          <div>
+            <img
+              src="../assets/images/landing-page/phone.png"
+              alt="phone"
+              width="60px"
+            />
+            <h5>24/7 Support</h5>
+            <p>
+              We have 24/7 contact support so you can contact us whenever you
+              want and we will respond it.
+            </p>
+          </div>
+
+          <div>
+            <img
+              src="../assets/images/landing-page/phone.png"
+              alt="phone"
+              width="60px"
+            />
+            <h5>24/7 Support</h5>
+            <p>
+              We have 24/7 contact support so you can contact us whenever you
+              want and we will respond it.
+            </p>
+          </div>
+        </div>
+      </section>
 
       {/* FEATURES */}
-      <section>Features</section>
+      <section style={{ background: "rgba(71, 58, 209, 0.06)" }}>
+        <div className="container d-flex">
+          <img
+            src="../assets/images/landing-page/feature.png"
+            alt="image"
+            width="450px"
+          />
+
+          <div>
+            <h1>All The Great Zwallet Features.</h1>
+
+            <div>
+              <h5>1. Small Fee</h5>
+              <p>
+                We only charge 5% of every success transaction done in Zwallet
+                app.
+              </p>
+            </div>
+
+            <div>
+              <h5>2. Small Fee</h5>
+              <p>
+                We only charge 5% of every success transaction done in Zwallet
+                app.
+              </p>
+            </div>
+
+            <div>
+              <h5>3. Small Fee</h5>
+              <p>
+                We only charge 5% of every success transaction done in Zwallet
+                app.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
 
       {/* COMMENT */}
-      <section>Comment</section>
+      <section className="container">
+        <h1>What Users are Saying.</h1>
+        <p>
+          We have some great features from the application and it’s totally free
+          to use by all users around the world.
+        </p>
+
+        <div className="d-flex">
+          <div>
+            <h5>Sherina Chaw</h5>
+            <p>
+              “I use this app since 2 years ago and this is the best app that
+              I’ve ever use in my entire life”
+            </p>
+          </div>
+
+          <div>
+            <h5>Sherina Chaw</h5>
+            <p>
+              “I use this app since 2 years ago and this is the best app that
+              I’ve ever use in my entire life”
+            </p>
+          </div>
+
+          <div>
+            <h5>Sherina Chaw</h5>
+            <p>
+              “I use this app since 2 years ago and this is the best app that
+              I’ve ever use in my entire life”
+            </p>
+          </div>
+        </div>
+      </section>
 
       {/* FOOTER */}
       <section style={{ background: "#6379F4" }}>
