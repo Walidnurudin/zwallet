d7e18cfd8e63c1c0548d13056b47dbbc9a73acc8
Walidnurudin <walidnurudin47@gmail.com>
2021-11-23 00:10:55 +0700

    Slicing: landing-page {dekstop)

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,13 @@ export default function Home() {
   const router = useRouter();
 
   return (
-    <div style={{ backgroundColor: "#E5E5E5" }}>
+    <div style={{ background: "rgba(99, 121, 244, 0.2)" }}>
       {/* HEADER */}
-      <section className="container d-flex justify-content-between">
-        <img src="../assets/images/logo/zwallet.png" alt="logo" />
+      <section
+        className="container d-flex justify-content-between"
+        style={{ padding: "50px 0px" }}
+      >
+        <img src="../assets/images/logo/zwallet.png" alt="logo" width="98px" />
 
         <div>
           {Cookie.get("token") ? (
@@ -41,38 +44,61 @@ export default function Home() {
       </section>
 
       {/* HERO */}
-      <section className="container d-flex">
+      <section
+        className="container d-flex gap-5"
+        style={{ marginBottom: "110px" }}
+      >
         <img
           src="../assets/images/landing-page/hero.png"
           alt="hero"
           width="460px"
         />
 
-        <div>
-          <div>
-            <p>Awesome App</p>
-            <p>
-              For Saving <span>Time.</span>
-            </p>
+        <div className="align-self-center">
+          <h1
+            className="nunito-700"
+            style={{
+              marginBottom: "40px",
+              lineHeight: "93px",
+              fontSize: "60px",
+            }}
+          >
+            Awesome App <br /> For Saving{" "}
+            <span className="font-primary">Time.</span>
+          </h1>
+
+          <div className="nunito-400 ">
+            We bring you a mobile app for banking problems that
+            <br /> oftenly wasting much of your times.
           </div>
 
-          <div>
-            We bring you a mobile app for banking problems that oftenly wasting
-            much of your times.
-          </div>
+          <button
+            style={{
+              margin: "50px 0px",
+              backgroundColor: "#6379f4",
+              padding: "16px 0px",
+              color: "white",
+              border: "none",
+              width: "173px",
+              borderRadius: "12px",
+            }}
+            className="nunito-700"
+          >
+            Try It Free
+          </button>
 
-          <button className="btn btn-primary">Try It Free</button>
-
-          <p>Available on</p>
+          <p className="nunito-400">Available on</p>
 
           <div>
             <img
               src="../assets/images/landing-page/playstore.png"
               alt="playstore"
+              width="50px"
             />
             <img
               src="../assets/images/landing-page/appstore.png"
               alt="appstore"
+              width="50px"
             />
           </div>
         </div>
@@ -117,61 +143,100 @@ export default function Home() {
       </section>
 
       {/* ABOUT */}
-      <section className="container text-center">
-        <h1 className="nunito-700">
+      <section
+        className="container text-center"
+        style={{ padding: "120px 0px" }}
+      >
+        <h1
+          className="nunito-700"
+          style={{
+            marginBottom: "30px",
+            lineHeight: "93px",
+            fontSize: "60px",
+          }}
+        >
           <span className="font-primary">About</span> the Application.
         </h1>
 
-        <p>
+        <p className="nunito-400" style={{ marginBottom: "70px" }}>
           We have some great features from the application and it’s totally free
           to use by all users around the world.
         </p>
 
-        <div className="d-flex">
-          <div>
+        <div className="d-flex gap-5">
+          <div
+            style={{
+              background: "#FFFFFF",
+              boxShadow: "0px 4px 250px rgba(172, 172, 172, 0.15)",
+              borderRadius: "25px",
+              padding: "40px 30px 50px 30px",
+            }}
+          >
             <img
               src="../assets/images/landing-page/phone.png"
               alt="phone"
-              width="60px"
+              width="80px"
             />
-            <h5>24/7 Support</h5>
-            <p>
+            <h5 className="nunito-700" style={{ margin: "30px 0px" }}>
+              24/7 Support
+            </h5>
+            <p className="nunito-400 font-secondary">
               We have 24/7 contact support so you can contact us whenever you
               want and we will respond it.
             </p>
           </div>
 
-          <div>
+          <div
+            style={{
+              background: "#FFFFFF",
+              boxShadow: "0px 4px 250px rgba(172, 172, 172, 0.15)",
+              borderRadius: "25px",
+              padding: "40px 30px 50px 30px",
+            }}
+          >
             <img
-              src="../assets/images/landing-page/phone.png"
+              src="../assets/images/landing-page/lock.png"
               alt="phone"
-              width="60px"
+              width="80px"
             />
-            <h5>24/7 Support</h5>
-            <p>
-              We have 24/7 contact support so you can contact us whenever you
-              want and we will respond it.
+            <h5 className="nunito-700" style={{ margin: "30px 0px" }}>
+              Data Privacy
+            </h5>
+            <p className="nunito-400 font-secondary">
+              We make sure your data is safe in our database and we will encrypt
+              any data you submitted to us.
             </p>
           </div>
 
-          <div>
+          <div
+            style={{
+              background: "#FFFFFF",
+              boxShadow: "0px 4px 250px rgba(172, 172, 172, 0.15)",
+              borderRadius: "25px",
+              padding: "40px 30px 50px 30px",
+            }}
+          >
             <img
-              src="../assets/images/landing-page/phone.png"
+              src="../assets/images/landing-page/download.png"
               alt="phone"
-              width="60px"
+              width="80px"
             />
-            <h5>24/7 Support</h5>
-            <p>
-              We have 24/7 contact support so you can contact us whenever you
-              want and we will respond it.
+            <h5 className="nunito-700" style={{ margin: "30px 0px" }}>
+              Easy Download
+            </h5>
+            <p className="nunito-400 font-secondary">
+              Zwallet is 100% totally free to use it’s now available on Google
+              Play Store and App Store.
             </p>
           </div>
         </div>
       </section>
 
       {/* FEATURES */}
-      <section style={{ background: "rgba(71, 58, 209, 0.06)" }}>
-        <div className="container d-flex">
+      <section
+        style={{ background: "rgba(71, 58, 209, 0.06)", padding: "80px 0px" }}
+      >
+        <div className="container d-flex align-items-center flex-md-row flex-column gap-5">
           <img
             src="../assets/images/landing-page/feature.png"
             alt="image"
@@ -179,29 +244,69 @@ export default function Home() {
           />
 
           <div>
-            <h1>All The Great Zwallet Features.</h1>
+            <h1
+              className="nunito-700"
+              style={{
+                marginBottom: "40px",
+                lineHeight: "93px",
+                fontSize: "60px",
+              }}
+            >
+              All The <span className="font-primary">Great</span>
+              <br /> Zwallet Features.
+            </h1>
 
-            <div>
-              <h5>1. Small Fee</h5>
-              <p>
+            <div
+              style={{
+                padding: "25px",
+                background: "#FFFFFF",
+                boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.05)",
+                borderRadius: "25px",
+                marginBottom: "30px",
+              }}
+            >
+              <h5>
+                <span className="font-primary">1.</span> Small Fee
+              </h5>
+              <p className="m-0 nunito-400 font-secondary">
                 We only charge 5% of every success transaction done in Zwallet
                 app.
               </p>
             </div>
 
-            <div>
-              <h5>2. Small Fee</h5>
-              <p>
-                We only charge 5% of every success transaction done in Zwallet
-                app.
+            <div
+              style={{
+                padding: "25px",
+                background: "#FFFFFF",
+                boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.05)",
+                borderRadius: "25px",
+                marginBottom: "30px",
+              }}
+            >
+              <h5>
+                <span className="font-primary">2.</span> Data Secured
+              </h5>
+              <p className="m-0 nunito-400 font-secondary">
+                All your data is secured properly in our system and it’s
+                encrypted.
               </p>
             </div>
 
-            <div>
-              <h5>3. Small Fee</h5>
-              <p>
-                We only charge 5% of every success transaction done in Zwallet
-                app.
+            <div
+              style={{
+                padding: "25px",
+                background: "#FFFFFF",
+                boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.05)",
+                borderRadius: "25px",
+                marginBottom: "30px",
+              }}
+            >
+              <h5>
+                <span className="font-primary">2.</span> User Friendly
+              </h5>
+              <p className="m-0 nunito-400 font-secondary">
+                Zwallet come up with modern and sleek design and not
+                complicated.
               </p>
             </div>
           </div>
@@ -209,47 +314,94 @@ export default function Home() {
       </section>
 
       {/* COMMENT */}
-      <section className="container">
-        <h1>What Users are Saying.</h1>
-        <p>
+      <section className="container" style={{ padding: "100px 0px" }}>
+        <h1 className="nunito-700 text-center mb-5">
+          What Users are <span className="font-primary">Saying.</span>
+        </h1>
+        <p className="text-center nunito-400 font-secondary mb-5">
           We have some great features from the application and it’s totally free
           to use by all users around the world.
         </p>
 
-        <div className="d-flex">
-          <div>
-            <h5>Sherina Chaw</h5>
-            <p>
+        <div className="d-flex flex-md-row flex-column gap-md-5 gap-3">
+          <div
+            style={{
+              borderRadius: "25px",
+              boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.05)",
+              padding: "50px 30px",
+              background: "#fff",
+            }}
+            className="text-center"
+          >
+            <img
+              src="../assets/images/landing-page/user1.png"
+              alt="user"
+              width="60px"
+            />
+            <h5 style={{ margin: "35px 0px" }} className="nunito-700">
+              Sherina Chaw
+            </h5>
+            <p className="nunito-400 font-secondary">
               “I use this app since 2 years ago and this is the best app that
               I’ve ever use in my entire life”
             </p>
           </div>
 
-          <div>
-            <h5>Sherina Chaw</h5>
-            <p>
-              “I use this app since 2 years ago and this is the best app that
-              I’ve ever use in my entire life”
+          <div
+            style={{
+              borderRadius: "25px",
+              boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.05)",
+              padding: "50px 30px",
+              background: "#fff",
+            }}
+            className="text-center"
+          >
+            <img
+              src="../assets/images/landing-page/user2.png"
+              alt="user"
+              width="60px"
+            />
+            <h5 style={{ margin: "35px 0px" }} className="nunito-700">
+              Jessica Mera
+            </h5>
+            <p className="nunito-400 font-secondary">
+              “I use Zwallet to manage all financial needs. It’s super easy to
+              use and it’s 100% free app”
             </p>
           </div>
 
-          <div>
-            <h5>Sherina Chaw</h5>
-            <p>
-              “I use this app since 2 years ago and this is the best app that
-              I’ve ever use in my entire life”
+          <div
+            style={{
+              borderRadius: "25px",
+              boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.05)",
+              padding: "50px 30px",
+              background: "#fff",
+            }}
+            className="text-center"
+          >
+            <img
+              src="../assets/images/landing-page/user3.png"
+              alt="user"
+              width="60px"
+            />
+            <h5 style={{ margin: "35px 0px" }} className="nunito-700">
+              Robert Chandler
+            </h5>
+            <p className="nunito-400 font-secondary">
+              “Since I’m using this app, I’m not going to move to another
+              similar app. Thank you Zwallet!”
             </p>
           </div>
         </div>
       </section>
 
       {/* FOOTER */}
-      <section style={{ background: "#6379F4" }}>
+      <section style={{ background: "#6379F4", padding: "50px 0px" }}>
         <div className="container">
           <div className="d-flex justify-content-between">
             <div>
               <img src="../assets/images/logo/zwallet2.png" alt="logo" />
-              <p>
+              <p className="nunito-400 text-white py-5 m-0 w-50">
                 Simplify financial needs and saving much time in banking needs
                 with one single app.
               </p>
@@ -258,12 +410,12 @@ export default function Home() {
             <div></div>
           </div>
 
-          <hr />
+          <hr style={{ color: "white" }} />
 
-          <div className="d-flex justify-content-between">
+          <div className="d-flex justify-content-between nunito-400 text-white pt-3">
             <p>2020 Zwallet. All right reserved.</p>
 
-            <div>
+            <div className="d-flex flex-md-row flex-column gap-md-5">
               <p>+62 5637 8882 9901</p>
               <p>contact@zwallet.com</p>
             </div>
